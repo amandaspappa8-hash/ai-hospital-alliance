721a1d593d0e2cbdc4454793e0d0383ddf2e334b
Mohammed Elfallah <mohammed@carebot.ai>
2026-05-06 19:09:44 +0200

    remove duplicate auth.js to use auth.ts

--- backend/app/db.py
+++ backend/app/db.py
@@ -17,6 +17,8 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.q0b7iqqhvug"
+    "revision": "0.7jug9e4ribo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
